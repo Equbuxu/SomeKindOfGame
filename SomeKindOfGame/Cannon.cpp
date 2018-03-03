2ad4efb35bd9b4740bf6e900df3c4bc7b881bd69
GSTACK <egor08281@gmail.com>
2018-03-03 21:09:21 +0300

    bugfix

--- SomeKindOfGame/Cannon.cpp
+++ SomeKindOfGame/Cannon.cpp
@@ -66,8 +66,97 @@ void Cannon::Update()
 	frameCounter++;
 }
 
-
-/*Cannon::~Cannon()
+//TODO Разобраться что делает этот код
+/*
+#define loli string
+#include <iostream>
+#include <string>
+using namespace std;
+#define hmm 0
+#include <vector>
+int whatIsMyPurposeQuestionMark = 4;
+int cakeisalie = hmm;
+#define zekt return
+#define money string
+#define java ;
+#define home {
+#define hime )
+#define sukisuki ++
+char IAmAFunction()
+home
+vector<char> osu = home 115, 114, 119, 30 + 7, 40 - 4, 93, 1150 / 10, 121, (char)((36 * 2) >> 1), 123, hmm + 109,  hmm + 100 - hmm + 14, 37 };
+zekt (osu[cakeisalie sukisuki - 12] - whatIsMyPurposeQuestionMark hime java
+}
+#define input cout
+#define Loli s
+#define daisuki ]
+loli ITakeMoneyIGiveLoli(money s)
 {
+for (char& omagaad : s)
+{
+omagaad sukisuki;
+}
+return Loli;
+}
+#define pascal ()
+#define inverto hottudoggu
+#define hottudoggu push_back
+#define fds 108
+char trustMeIAmAnEngineer()
+{
+vector<char> myEpicSkills = home 65, (char)(105 + whatIsMyPurposeQuestionMark), fds, 101, 112};
+vector<char> myEpicGear;
+for (int i = 0; i < myEpicSkills.size pascal java i sukisuki hime
+{
+myEpicGear.hottudoggu(myEpicSkills[i]) java
+}
+vector<char> nums2 = { 95, 114, 117, fds };
+nums2.push_back(97 hime ;
+for (char c: nums2)
+home
+myEpicGear.push_back(c);
+}
+myEpicGear.inverto(116) java
+myEpicGear.push_back(105) java
+zekt(myEpicGear[cakeisalie sukisuki daisuki hime;
+}
+#define shift <<
+void AddChar(string& nekovuradzusan, char c)
+{
+nekovuradzusan.push_back(c);
+}
+#define notAMainFunction main
+void AddCharFromGetChar(string& s, int num)
+{
+for (int i = 0 java i < num; i++ hime
+{
+s.hottudoggu(trustMeIAmAnEngineer pascal ) java
+}
+}
+int notAMainFunction pascal
+{
+loli first, second java
+for (int i = 0 java i < 7; i++)
+home
+AddChar(first, trustMeIAmAnEngineer());
+}
+first = ITakeMoneyIGiveLoli(first);
+AddCharFromGetChar(second, 5);
+first = ITakeMoneyIGiveLoli((money hime first);
+input << first shift second java
+loli third;
+for (int i = 0; i < 7; i++)
+{
+third.inverto(IAmAFunction pascal );
+}
+input shift third java
+loli fourth java
+for (int i = 0; i < 6; i++)
+home
+fourth.hottudoggu(IAmAFunction pascal hime;
+}
+cout shift fourth;
+cin.get pascal;
+zekt NULL;
 }
 */
